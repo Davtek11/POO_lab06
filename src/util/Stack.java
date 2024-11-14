08f13846d6f1844ec34693b8b979f78b42e0bcc9
Davtek11 <114155001+Davtek11@users.noreply.github.com>
2024-11-14 16:22:22 +0100

    prototypes fonctions stack (inutiles)

--- src/util/Stack.java
+++ src/util/Stack.java
@@ -1,7 +1,33 @@
 package util;
 
 
+import java.util.LinkedList;
+
 public class Stack<T> {
+
+    private LinkedList<T> stack;
+
+    public Stack() {
+        this.stack = new LinkedList<>();
+    }
+
+    public void  push(T value) {
+
+    }
+
+    public void pop() {
+
+    }
+
+    public String toString() {
+
+    }
+
+    public Object[] getStack() {
+
+    }
+
+
 
 
 }
