d46239253a5f504bbbd5fe16aa29734dad728fe2
sara <sara.camassa@heig-vd.ch>
2024-11-19 10:17:30 +0100

    stack

---
+++ src/util/Stack.java
@@ -0,0 +1,35 @@
+package util;
+
+
+import java.util.LinkedList;
+
+public class Stack<T> {
+
+    public Stack() {
+        
+    }
+
+    public void  push(T value) {
+
+    }
+
+    public void pop() {
+
+    }
+
+    public String toString() {
+        return "";
+    }
+
+    public boolean empty() {
+        return false;
+    }
+
+    public Object[] getStack() {
+        return null;
+    }
+
+
+
+
+}
