a5fe57664eb307516572df94a096d0ebd924c71b
Davtek11 <114155001+Davtek11@users.noreply.github.com>
2024-11-19 11:46:39 +0100

    push Stack + iterator

--- src/util/Stack.java
+++ src/util/Stack.java
@@ -2,28 +2,67 @@ package util;
 
 public class Stack<T> {
 
+    private Node<T> top;
+    private int size;
+
     public Stack() {
-        
+        top = null;
+        size = 0;
     }
 
+    //add a value to the stack
     public void push(T value) {
-
+        top = new Node<>(value, top);
+        size++;
     }
 
+    //delete the top value
     public void pop() {
-
+        top = top.next;
+        size--;
     }
 
+    //return the value of the top Node
+    public T top() {
+        return top.value;
+    }
+
+    //return the stack in a string
     public String toString() {
-        return "";
+        if (empty()){
+            return "";
+        }
+
+        //String builder est mutable
+        StringBuilder s = new StringBuilder();
+        StackIterator<T> iterator = new StackIterator<>(top);
+        while (iterator.hasNext()) {
+            s.append(String.valueOf(iterator.next())).append(" ");
+        }
+        //trim() supprime l'espace final
+        return s.toString().trim();
     }
 
+
+    //is the stack empty
     public boolean empty() {
-        return false;
+        return (top == null);
     }
 
+    //return an Object tab composed of the stack values
     public Object[] getStack() {
-        return null;
+        if (empty()) {
+            return new Object[0];
+            //return null;
+        }
+        Object[] stack = new Object[size];
+        StackIterator<T> iterator = new StackIterator<>(top);
+        int index = 0;
+        while (iterator.hasNext()) {
+            stack[index++] = iterator.next();
+        }
+
+        return stack;
     }
 
 }
